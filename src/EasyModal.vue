301502f692b8f605450165a5c323f709facedf83
jwma <mjw@majiawei.com>
2017-05-12 15:45:49 +0800

    1. 加入 npm watch 命令 2. 添加 modal 基本行为

--- src/EasyModal.vue
+++ src/EasyModal.vue
@@ -1,11 +1,49 @@
+<style>
+.em__mask {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, .7);
+    z-index: 9998;
+}
+
+.em__body {
+    position: absolute;
+    z-index: 9999;
+    top: 20%;
+    left: 0; right: 0;
+    background-color: #fff;
+    border-radius: 10px;
+    width: 360px;
+    padding: 20px 0;
+    margin: 0 auto;
+    text-align: center;
+}
+</style>
 <template>
-    <div class="em__container">
-        <div class="em__mask">蒙层</div>
-        <div class="em__body">主体</div>
+    <div class="em__container" v-show="show">
+        <div class="em__mask" @click="clickMask"></div>
+        <div class="em__body">
+            <slot name="body">Easy modal</slot>
+        </div>
     </div>
 </template>
 <script>
 export default {
-    name: 'EasyModal'
+    name: 'EasyModal',
+    props: {
+        show: {
+            type: Boolean,
+            required: true,
+            default: false
+        }
+    },
+    methods: {
+        clickMask: function() {
+            this.$emit('click-mask')
+        }
+    }
 }
 </script>
